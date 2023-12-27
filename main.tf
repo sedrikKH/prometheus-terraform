52ed05b1bde73bf0ca49fb4dfa4665b312b10045
Sergiy <adamchuk.mail@gmail.com>
2023-12-27 09:06:30 +0000

    some changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/sedrikKH/prometheus-terraform/tf-google-gke-cluster"
+  source         = "github.com/sedrikKH/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
