512ea4b61c21fcc9d71c2e2e36c79febacee0e88
Sergiy <adamchuk.mail@gmail.com>
2023-12-29 13:43:35 +0000

    ch

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "prom-tfstate"
-#     prefix = "terraform/state"
-#   }
-# }
-
 module "github_repository" {
   source = "github.com/den-vasyliev/tf-github-repository"
   github_owner = var.GITHUB_OWNER
@@ -15,28 +8,37 @@ module "github_repository" {
 }
 
 
-module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
-}
+# module "kind_cluster" {
+#   source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+# }
 
-# module "gke_cluster" {
-#   source         = "github.com/sedrikKH/tf-google-gke-cluster"
-#   GOOGLE_REGION  = var.GOOGLE_REGION
-#   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-#   GKE_NUM_NODES  = var.GKE_NUM_NODES
-#   GKE_MACHINE_TYPE    = var.GKE_MACHINE_TYPE
-#   GKE_CLUSTER_NAME    = var.GKE_CLUSTER_NAME
-#   }
+module "gke_cluster" {
+  source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+  GKE_MACHINE_TYPE    = var.GKE_MACHINE_TYPE
+  GKE_CLUSTER_NAME    = var.GKE_CLUSTER_NAME
+  }
 
 
 module "flux_boostrap" {
-  source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+  # For GKE cluster
+  source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  config_host       = module.gke_cluster.config_host
+  config_token       = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
+  
+  # For kind cluster
+  #source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+  #config_host       = module.kind_cluster.endpoint
+  #config_client_key = module.kind_cluster.client_key
+  #config_ca         = module.kind_cluster.ca
+  #config_crt        = module.kind_cluster.crt
+  
+  
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key = module.tls_private_key.private_key_pem
-  config_host       = module.kind_cluster.endpoint
-  config_client_key = module.kind_cluster.client_key
-  config_ca         = module.kind_cluster.ca
-  config_crt        = module.kind_cluster.crt
   github_token      = var.GITHUB_TOKEN
   #config_path = module.gke_cluster.kubeconfig
 }
---
+++ terraform.tf
@@ -0,0 +1,6 @@
+# terraform {
+#   backend "gcs" {
+#     bucket = "prom-tfstate"
+#     prefix = "terraform/state"
+#   }
+# }
