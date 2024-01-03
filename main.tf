858e6e1d6b6f59184444aae8742b4278061e819c
Sergiy <adamchuk.mail@gmail.com>
2024-01-03 09:30:55 +0000

    ch

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "github_repository" {
-  source = "github.com/den-vasyliev/tf-github-repository"
+  #source = "github.com/den-vasyliev/tf-github-repository"
+  source = "./modules/github_repository"
   github_owner = var.GITHUB_OWNER
   github_token = var.GITHUB_TOKEN
   repository_name = var.FLUX_GITHUB_REPO
@@ -8,39 +9,40 @@ module "github_repository" {
 }
 
 
-module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
-}
+# module "kind_cluster" {
+#   source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+# }
 
-# module "gke_cluster" {
-#   source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
-#   GOOGLE_REGION  = var.GOOGLE_REGION
-#   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-#   GKE_NUM_NODES  = var.GKE_NUM_NODES
-#   GKE_MACHINE_TYPE    = var.GKE_MACHINE_TYPE
-#   GKE_CLUSTER_NAME    = var.GKE_CLUSTER_NAME
-#   }
+module "gke_cluster" {
+  source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+  GKE_MACHINE_TYPE    = var.GKE_MACHINE_TYPE
+  GKE_CLUSTER_NAME    = var.GKE_CLUSTER_NAME
+  }
 
 
 module "flux_boostrap" {
   # For GKE cluster
-  # source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
-  # config_host       = module.gke_cluster.config_host
-  # config_token       = module.gke_cluster.config_token
-  # config_ca         = module.gke_cluster.config_ca
+  source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  config_host       = module.gke_cluster.config_host
+  config_token       = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
   
   # For kind cluster
-  source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
-  config_host       = module.kind_cluster.endpoint
-  config_client_key = module.kind_cluster.client_key
-  config_ca         = module.kind_cluster.ca
-  config_crt        = module.kind_cluster.crt
+  # source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+  # config_host       = module.kind_cluster.endpoint
+  # config_client_key = module.kind_cluster.client_key
+  # config_ca         = module.kind_cluster.ca
+  # config_crt        = module.kind_cluster.crt
   
   
-  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  github_repository = "${var.GITHUB_OWNER}/${module.github_repository.name}"
+  #github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key = module.tls_private_key.private_key_pem
   github_token      = var.GITHUB_TOKEN
-  #config_path = module.gke_cluster.kubeconfig
+  #config_path = module.gke_cluster.gke_auth.kubeconfig_raw
 
 }
 
