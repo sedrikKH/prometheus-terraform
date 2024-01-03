c3bd78370edac8aa07e682161d63b8518f447516
Sergiy <adamchuk.mail@gmail.com>
2024-01-03 13:46:45 +0000

    ppp

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ module "github_repository" {
 # }
 
 module "gke_cluster" {
-  source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
+  source         = "./modules/gke_cluster"
+  #source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
